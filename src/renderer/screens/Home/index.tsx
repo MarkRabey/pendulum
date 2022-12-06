0041ae925cb357df611c384d337df70fd47c4ca7
Mark Rabey <mark.rabey@bounteous.com>
2022-12-06 12:03:18 -0500

    refactor to tray

--- src/renderer/screens/Home/index.tsx
+++ src/renderer/screens/Home/index.tsx
@@ -1,19 +1,11 @@
 import { Flex } from '@chakra-ui/react';
-import IntervalSelector from 'renderer/components/IntervalSelector';
 import TimerControls from 'renderer/components/TimerControls';
 import TimerDisplay from 'renderer/components/TimerDisplay';
 import Layout from 'renderer/containers/Layout';
 import { useTimerContext } from 'renderer/context/TimerContext';
 
 const Home = () => {
-  const {
-    time,
-    timerRunning,
-    toggleTimer,
-    handleReset,
-    selectedTime,
-    handleSetTime,
-  } = useTimerContext();
+  const { time, timerRunning, toggleTimer, handleReset } = useTimerContext();
   return (
     <Layout>
       <Flex direction="column" alignItems="center">
@@ -22,10 +14,6 @@ const Home = () => {
           timerRunning={timerRunning}
           toggleTimer={toggleTimer}
           handleReset={handleReset}
-        />
-        <IntervalSelector
-          selectedTime={selectedTime}
-          handleSetTime={handleSetTime}
         />
       </Flex>
     </Layout>
