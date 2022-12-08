6e1dd379b1286475055a91472dbf342c42eee9f2
Mark Rabey <mark.rabey@bounteous.com>
2022-12-08 10:24:15 -0500

    Add electron mocks for testing

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -3,6 +3,21 @@ import { render } from '@testing-library/react';
 import App from '../renderer/App';
 
 describe('App', () => {
+  global.window.electron = {
+    minimize: jest.fn(),
+    maximize: jest.fn(),
+    close: jest.fn(),
+    setTime: jest.fn(),
+    sendMessage: jest.fn(),
+    storeSettings: jest.fn(),
+    saveSetting: jest.fn(),
+    getSettings: jest.fn(),
+    ipcRenderer: {
+      sendMessage: jest.fn(),
+      on: jest.fn(),
+      once: jest.fn(),
+    },
+  };
   it('should render', () => {
     expect(render(<App />)).toBeTruthy();
   });
